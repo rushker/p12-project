84dae93a0903547c468dc5f35d3e87dea07ebd1d
rushker <altiosle102@gmail.com>
2025-04-04 10:21:31 +0700

    fix quick

--- client/src/pages/QRViewPage.jsx
+++ client/src/pages/QRViewPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import QRCode from 'react-qr-code'; // QR Code library to render QR codes
 import { useParams } from 'react-router-dom';
 import '../styles/QRViewPage.css';
 
